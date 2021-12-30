45189b368e70d41f2b62d09832f59480819037e8
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-12-30 07:06:54 -0500

    setup Dagger

---
+++ app/src/main/java/ca/josue/notesapp/ui/di/AppModule.kt
@@ -0,0 +1,46 @@
+package ca.josue.notesapp.ui.di
+
+import android.app.Application
+import androidx.room.Room
+import ca.josue.notesapp.ui.feature_note.data.data_source.NoteDatabase
+import ca.josue.notesapp.ui.feature_note.data.repository.NoteRepositoryImpl
+import ca.josue.notesapp.ui.feature_note.domain.repository.NoteRepository
+import ca.josue.notesapp.ui.feature_note.domain.use_cas.DeleteNote
+import ca.josue.notesapp.ui.feature_note.domain.use_cas.GetNotes
+import ca.josue.notesapp.ui.feature_note.domain.use_cas.NoteUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app : Application) : NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db : NoteDatabase) : NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCase(repository : NoteRepository) : NoteUseCase {
+        return NoteUseCase(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+
+}
--- app/src/main/java/ca/josue/notesapp/ui/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/ca/josue/notesapp/ui/feature_note/data/data_source/NoteDatabase.kt
@@ -11,4 +11,8 @@ import ca.josue.notesapp.ui.feature_note.domain.model.Note
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract val noteDao : NoteDao
+
+    companion object{
+        const val DATABASE_NAME = "notes_db"
+    }
 }
