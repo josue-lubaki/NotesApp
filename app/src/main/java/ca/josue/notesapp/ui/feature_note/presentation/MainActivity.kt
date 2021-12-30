197a099d11d56ed3e4ef49ea7d15b1a04b54c491
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-12-30 04:01:50 -0500

    clean architecture code

--- app/src/main/java/ca/josue/notesapp/ui/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/ca/josue/notesapp/ui/feature_note/presentation/MainActivity.kt
@@ -1,11 +1,19 @@
-package ca.josue.notesapp
+package ca.josue.notesapp.ui.feature_note.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import ca.josue.notesapp.R
+import ca.josue.notesapp.ui.theme.notesAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent{
+            notesAppTheme {
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/ca/josue/notesapp/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ca.josue.notesapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.Dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(Dp(4f)),
+    medium = RoundedCornerShape(Dp(4f)),
+    large = RoundedCornerShape(Dp(0f)),
+)
