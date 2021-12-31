eb9134971828bbb758bf88df4d17cb5a76e802e7
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-12-31 01:44:25 -0500

    Notes Screen compose

--- app/src/main/java/ca/josue/notesapp/ui/theme/Shape.kt
+++ app/src/main/java/ca/josue/notesapp/ui/theme/Shape.kt
@@ -2,10 +2,10 @@ package ca.josue.notesapp.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(Dp(4f)),
-    medium = RoundedCornerShape(Dp(4f)),
-    large = RoundedCornerShape(Dp(0f)),
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp),
 )
